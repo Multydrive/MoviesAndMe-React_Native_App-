148e0a2c89c6edcbf3ac04e7f416a20c903a277b
Andreas <andreas.basso@std.heh.be>
2020-11-02 21:22:03 +0100

    use a API to get informations on many movies

---
+++ API/TMDBApi.js
@@ -0,0 +1,12 @@
+const API_TOKEN = "52795092e25c8e2c344cc9b0b1b98d7d";
+
+export function getFilmsFromApiWithSearchedText (text,page) {
+  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + "&page=" + page
+  return fetch(url)
+    .then((response) => response.json())
+    .catch((error) => console.error(error))
+}
+
+export function getImageFromApi (name) {
+  return 'https://image.tmdb.org/t/p/w300' + name
+}
